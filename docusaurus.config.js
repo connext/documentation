e56d32557c7831a4c1c40ea0f7229cf265edbd54
Rahul Sethuram <rahul@sethuram.com>
2021-07-12 11:52:42 +0300

    fix build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ module.exports = {
   tagline: "Cross Chain Infrastructure",
   url: "https://your-docusaurus-test-site.com",
   baseUrl: "/",
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
   organizationName: "facebook", // Usually your GitHub org/user name.
