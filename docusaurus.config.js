241b487068cc582c8178ddad7e5c044deab6f505
just-a-node <eye1717@gmail.com>
2022-08-29 11:06:54 -0600

    Theme switch for better visibility with code highlights

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,5 @@
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require("prism-react-renderer/themes/okaidia");
+const darkCodeTheme = require("prism-react-renderer/themes/okaidia");
 const CardLink = require("./src/remark/CardLink");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
