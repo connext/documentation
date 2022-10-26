eaaea0e8962162ca13ae989ee92dad6f4dfafbca
just-a-node <eye1717@gmail.com>
2022-10-25 19:06:58 -0600

    Refactor logos, remove extra buttons

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,12 +6,13 @@ import styles from "./index.module.css";
 
 import styled from "@emotion/styled";
 
-import Discord from "/static/img/discord.svg";
+import DiscordLogo from "/static/img/discord_logo.svg";
+
+import GithubLogo from "/static/img/github_logo.svg";
 
 import ConnextLogo from "/static/img/connext_Logo.svg";
 
-import XCallCalldataSnippet from "/static/img/xcall_calldata_snippet.png"
-import XCallTransferSnippet from "/static/img/xcall_transfer_snippet.png"
+import XCallSnippet from "/static/img/xcall_snippet_v2.png"
 
 import ThemedImage from "@theme/ThemedImage";
 import useBaseUrl from "@docusaurus/useBaseUrl";
@@ -27,7 +28,7 @@ import {
   SwitchHorizontalIcon,
 } from "@heroicons/react/outline";
 
-export const actions = [
+export const Actions = [
   {
     title: "Core Concepts",
     href: "#",
@@ -58,18 +59,6 @@ export const actions = [
   },
 ];
 
-export const github = [
-  {
-    title: "xapp-starter",
-    href: "https://github.com/connext/xapp-starter",
-    icon: CodeIcon,
-  },
-  {
-    title: "nxtp",
-    href: "https://github.com/connext/nxtp",
-    icon: CodeIcon,
-  },
-];
 
 export const Guides = [
   {
@@ -122,7 +111,6 @@ const LongRow = styled.div`
   grid-gap: 16px;
   justify-content: center;
   margin: 0 auto;
-  padding: 1rem 0;
   max-width: 960px;
 
   @media (max-width: 1250px) {
@@ -159,7 +147,6 @@ const Card = styled.div`
   border: 1px solid transparent;
   border-radius: 20px;
   border: 1px solid var(--ifm-color-emphasis-200);
-  /* flex: 1 1 0px; */
   border-color: rgba(140, 127, 148, 0.5);
 
   &:hover {
@@ -179,6 +166,7 @@ const CTACard = styled.div`
   color: #9c62f9;
   max-height: 250px;
   padding: 1rem;
+  margin-top: 2rem;
   flex-direction: column;
   justify-content: center;
   cursor: pointer;
@@ -199,7 +187,9 @@ const CTACard = styled.div`
 `;
 
 const CenterCard = styled(Card)`
+  width: 100%;
   min-width: 250px;
+  margin: 1rem 0rem;
   justify-content: space-between;
   align-items: center;
   flex-direction: row;
@@ -279,12 +269,6 @@ const StyledTitleImage = styled(StyledImage)`
   mask-image: linear-gradient(rgba(0, 0, 0, 1), transparent);
 `;
 
-const StyledGithubIcon = styled.div`
-  svg {
-    fill: var(--ifm-font-color-base);
-  }
-`;
-
 const HideMedium = styled.div`
   @media (max-width: 960px) {
     display: none;
@@ -322,32 +306,23 @@ export default function Home() {
                   align: "left",
                 }}
               >
-                Seamlessly integrate cross-chain functionality into your application in just a few lines of code. 
+                Turn your dApp into a xApp with just a few lines of code. 
               </p>
 
-              <Link
-                style={{ textDecoration: "none" }}
-                to="./developers/quickstart"
-              >
+              <div>
+                <img src={XCallSnippet} alt="xCall Snippet" style={{borderRadius: "20px", width: "350px"}}  />
+              </div>
+
+              <Link to="./developers/quickstart">
                 <CTACard>
-                  Get Started!
+                   Get Started!
                 </CTACard>
               </Link>
-
-              <TwoRow>
-                <div>
-                  <img src={XCallCalldataSnippet} alt="xCall Snippet" style={{borderRadius: "20px", width: "350px"}}  />
-                </div>
-                <div>
-                  <img src={XCallTransferSnippet} alt="xCall Snippet" style={{borderRadius: "20px", width: "350px"}} />
-                </div>
-              </TwoRow>
           </div>
 
           <LongRow>
-            {actions.map((action) => (
+            {Actions.map((action) => (
               <Link
-                style={{ textDecoration: "none" }}
                 to={action.to}
                 key={action.to}
               >
@@ -386,233 +361,103 @@ export default function Home() {
           }}
         >
           <div>
-            <h2>Getting Started</h2>
+            <h2>Ecosystem Links</h2>
             <p>
-              Explore these docs to start integrating the Connext Protocol in
-              your project.
+              Explore the Connext ecosystem.
             </p>
             <div>
-              {Guides.map((action) => (
-                <Link
-                  style={{ textDecoration: "none" }}
-                  key={action.title}
-                  to={action.to}
-                >
-                  <Card key={action.title} style={{ marginBottom: "1rem" }}>
-                    <LinkRow>
-                      <div style={{ display: "flex", alignItems: "center" }}>
-                        <h3 style={{ marginBottom: "0rem" }}>{action.title}</h3>
-                      </div>
-                      <svg
-                        style={{ width: "24px", opacity: 0.2 }}
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 24 24"
-                      >
-                        <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-                      </svg>
-                    </LinkRow>
-                    <p style={{ marginBottom: "0rem" }}>{action.text}</p>
-                  </Card>
-                </Link>
-              ))}
+              <Link
+                href={"https://bridge.connext.network/"}
+              >
+                <CenterCard>
+                  <ConnextLogo style={{ width: "48px", height: "48px" }} />
+                  <div>
+                    <h3>Bridge</h3>
+                    <p>The official Connext bridge UI.</p>
+                  </div>
+                </CenterCard>
+              </Link>
+              <Link
+                href={"https://connextscan.io/"}
+              >
+                <CenterCard>
+                  <ConnextLogo style={{ width: "48px", height: "48px" }} />
+                  <div>
+                    <h3>Connextscan</h3>
+                    <p>Explore transactions and liquidity information.</p>
+                  </div>
+                </CenterCard>
+              </Link>
+              <Link
+                href={"https://discord.gg/zZGTd9wT"}
+              >
+                <CenterCard>
+                  <DiscordLogo style={{ width: "48px", height: "48px" }} />
+                  <div>
+                    <h3>Discord</h3>
+                    <p>Join us in #dev-hub to get realtime help.</p>
+                  </div>
+                </CenterCard>
+              </Link>
+              <Link
+                href={"https://connext.network/"}
+              >
+                <CenterCard>
+                  <ConnextLogo style={{ width: "48px", height: "48px" }} />
+                  <div>
+                    <h3>Connext Homepage</h3>
+                    <p>Learn more about the Connext ecosystem.</p>
+                  </div>
+                </CenterCard>
+              </Link>
+              <Link
+                href={"https://medium.com/connext/"}
+              >
+                <CenterCard>
+                  <ConnextLogo style={{ width: "48px", height: "48px" }} />
+                  <div>
+                    <h3>Blog</h3>
+                    <p>Read about events, updates, and news.</p>
+                  </div>
+                </CenterCard>
+              </Link>
+
             </div>
           </div>
+
           <div>
             <h2>Developer Links</h2>
             <p>
               The Connext codebase is comprised of an ecosystem of open source
               components.
             </p>
-            {github.map((action) => (
-              <Link style={{ textDecoration: "none" }} href={action.href}>
-                <Card key={action.title} style={{ marginBottom: "1rem" }}>
-                  <LinkRow>
-                    <StyledGithubIcon
-                      style={{ display: "flex", alignItems: "center" }}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 120.78 117.79"
-                        style={{ width: "24px" }}
-                      >
-                        <defs></defs>
-                        <title>Logo</title>
-                        <g id="Layer_2" data-name="Layer 2">
-                          <g id="Layer_1-2" data-name="Layer 1">
-                            <path
-                              className="cls-1"
-                              d="M60.39,0A60.39,60.39,0,0,0,41.3,117.69c3,.56,4.12-1.31,4.12-2.91,0-1.44-.05-6.19-.08-11.24C28.54,107.19,25,96.42,25,96.42c-2.75-7-6.71-8.84-6.71-8.84-5.48-3.75.41-3.67.41-3.67,6.07.43,9.26,6.22,9.26,6.22,5.39,9.23,14.13,6.57,17.57,5,.55-3.9,2.11-6.56,3.84-8.07C36,85.55,21.85,80.37,21.85,57.23A23.35,23.35,0,0,1,28.08,41c-.63-1.52-2.7-7.66.58-16,0,0,5.07-1.62,16.61,6.19a57.36,57.36,0,0,1,30.25,0C87,23.42,92.11,25,92.11,25c3.28,8.32,1.22,14.46.59,16a23.34,23.34,0,0,1,6.21,16.21c0,23.2-14.12,28.3-27.57,29.8,2.16,1.87,4.09,5.55,4.09,11.18,0,8.08-.06,14.59-.06,16.57,0,1.61,1.08,3.49,4.14,2.9A60.39,60.39,0,0,0,60.39,0Z"
-                            />
-                            <path
-                              className="cls-2"
-                              d="M22.87,86.7c-.13.3-.6.39-1,.19s-.69-.61-.55-.91.61-.39,1-.19.69.61.54.91Z"
-                            />
-                            <path
-                              className="cls-2"
-                              d="M25.32,89.43c-.29.27-.85.14-1.24-.28a.92.92,0,0,1-.17-1.25c.3-.27.84-.14,1.24.28s.47,1,.17,1.25Z"
-                            />
-                            <path
-                              className="cls-2"
-                              d="M27.7,92.91c-.37.26-1,0-1.35-.52s-.37-1.18,0-1.44,1,0,1.35.51.37,1.19,0,1.45Z"
-                            />
-                            <path
-                              className="cls-2"
-                              d="M31,96.27A1.13,1.13,0,0,1,29.41,96c-.53-.49-.68-1.18-.34-1.54s1-.27,1.56.23.68,1.18.33,1.54Z"
-                            />
-                            <path
-                              className="cls-2"
-                              d="M35.46,98.22c-.15.47-.82.69-1.51.49s-1.13-.76-1-1.24.82-.7,1.51-.49,1.13.76,1,1.24Z"
-                            />
-                            <path
-                              className="cls-2"
-                              d="M40.4,98.58c0,.5-.56.91-1.28.92s-1.3-.38-1.31-.88.56-.91,1.29-.92,1.3.39,1.3.88Z"
-                            />
-                            <path
-                              className="cls-2"
-                              d="M45,97.8c.09.49-.41,1-1.12,1.12s-1.35-.17-1.44-.66.42-1,1.12-1.12,1.35.17,1.44.66Z"
-                            />
-                          </g>
-                        </g>
-                      </svg>
-                      <h3 style={{ marginBottom: "0rem", marginLeft: "16px" }}>
-                        {action.title}
-                      </h3>
-                    </StyledGithubIcon>
-                    <svg
-                      style={{ width: "24px", height: "24px", opacity: 0.2 }}
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-                    </svg>
-                  </LinkRow>
-                </Card>
+              <Link
+                href={"https://github.com/Connext"}
+              >
+                <CenterCard>
+                  <GithubLogo />
+                  <div>
+                    <h3>GitHub</h3>
+                    <p>View all Connext repositories.</p>
+                  </div>
+                </CenterCard>
               </Link>
-            ))}
           </div>
         </TwoRow>
-
-        <Row>
-          <Link
-            style={{ textDecoration: "none" }}
-            href={"https://github.com/Connext"}
-          >
-            <CenterCard>
-              <StyledGithubIcon>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 120.78 117.79"
-                  style={{ width: "48px" }}
-                >
-                  <defs></defs>
-                  <g id="Layer_2" data-name="Layer 2">
-                    <g id="Layer_1-2" data-name="Layer 1">
-                      <path
-                        className="cls-1"
-                        d="M60.39,0A60.39,60.39,0,0,0,41.3,117.69c3,.56,4.12-1.31,4.12-2.91,0-1.44-.05-6.19-.08-11.24C28.54,107.19,25,96.42,25,96.42c-2.75-7-6.71-8.84-6.71-8.84-5.48-3.75.41-3.67.41-3.67,6.07.43,9.26,6.22,9.26,6.22,5.39,9.23,14.13,6.57,17.57,5,.55-3.9,2.11-6.56,3.84-8.07C36,85.55,21.85,80.37,21.85,57.23A23.35,23.35,0,0,1,28.08,41c-.63-1.52-2.7-7.66.58-16,0,0,5.07-1.62,16.61,6.19a57.36,57.36,0,0,1,30.25,0C87,23.42,92.11,25,92.11,25c3.28,8.32,1.22,14.46.59,16a23.34,23.34,0,0,1,6.21,16.21c0,23.2-14.12,28.3-27.57,29.8,2.16,1.87,4.09,5.55,4.09,11.18,0,8.08-.06,14.59-.06,16.57,0,1.61,1.08,3.49,4.14,2.9A60.39,60.39,0,0,0,60.39,0Z"
-                      />
-                      <path
-                        className="cls-2"
-                        d="M22.87,86.7c-.13.3-.6.39-1,.19s-.69-.61-.55-.91.61-.39,1-.19.69.61.54.91Z"
-                      />
-                      <path
-                        className="cls-2"
-                        d="M25.32,89.43c-.29.27-.85.14-1.24-.28a.92.92,0,0,1-.17-1.25c.3-.27.84-.14,1.24.28s.47,1,.17,1.25Z"
-                      />
-                      <path
-                        className="cls-2"
-                        d="M27.7,92.91c-.37.26-1,0-1.35-.52s-.37-1.18,0-1.44,1,0,1.35.51.37,1.19,0,1.45Z"
-                      />
-                      <path
-                        className="cls-2"
-                        d="M31,96.27A1.13,1.13,0,0,1,29.41,96c-.53-.49-.68-1.18-.34-1.54s1-.27,1.56.23.68,1.18.33,1.54Z"
-                      />
-                      <path
-                        className="cls-2"
-                        d="M35.46,98.22c-.15.47-.82.69-1.51.49s-1.13-.76-1-1.24.82-.7,1.51-.49,1.13.76,1,1.24Z"
-                      />
-                      <path
-                        className="cls-2"
-                        d="M40.4,98.58c0,.5-.56.91-1.28.92s-1.3-.38-1.31-.88.56-.91,1.29-.92,1.3.39,1.3.88Z"
-                      />
-                      <path
-                        className="cls-2"
-                        d="M45,97.8c.09.49-.41,1-1.12,1.12s-1.35-.17-1.44-.66.42-1,1.12-1.12,1.35.17,1.44.66Z"
-                      />
-                    </g>
-                  </g>
-                </svg>{" "}
-              </StyledGithubIcon>
-              <div>
-                <h3>GitHub</h3>
-                <p>View all Connext repositories.</p>
-              </div>
-            </CenterCard>
-          </Link>
-          <Link
-            style={{ textDecoration: "none" }}
-            href={"https://bridge.connext.network/"}
-          >
-            <CenterCard>
-              <ConnextLogo style={{ width: "48px", height: "48px" }} />
-              <div>
-                <h3>Bridge</h3>
-                <p>The official Connext bridge UI.</p>
-              </div>
-            </CenterCard>
-          </Link>
-          <Link
-            style={{ textDecoration: "none" }}
-            href={"https://connextscan.io/"}
-          >
-            <CenterCard>
-              <ConnextLogo style={{ width: "48px", height: "48px" }} />
-              <div>
-                <h3>Connextscan</h3>
-                <p>Explore transactions and liquidity information.</p>
-              </div>
-            </CenterCard>
-          </Link>
-          <Link
-            style={{ textDecoration: "none" }}
-            href={"https://discord.gg/zZGTd9wT"}
-          >
-            <CenterCard>
-              <Discord style={{ width: "48px", height: "48px" }} />
-              <div>
-                <h3>Discord</h3>
-                <p>Join us in #dev-hub to get realtime help.</p>
-              </div>
-            </CenterCard>
-          </Link>
-          <Link
-            style={{ textDecoration: "none" }}
-            href={"https://connext.network/"}
-          >
-            <CenterCard>
-              <ConnextLogo style={{ width: "48px", height: "48px" }} />
-              <div>
-                <h3>Connext Homepage</h3>
-                <p>Learn more about the Connext ecosystem.</p>
-              </div>
-            </CenterCard>
-          </Link>
-          <Link
-            style={{ textDecoration: "none" }}
-            href={"https://medium.com/connext/"}
-          >
-            <CenterCard>
-              <ConnextLogo style={{ width: "48px", height: "48px" }} />
-              <div>
-                <h3>Blog</h3>
-                <p>Read about events, updates, and news.</p>
-              </div>
-            </CenterCard>
-          </Link>
-        </Row>
       </Container>
     </Layout>
   );
 }
+
+export const github = [
+  {
+    title: "xapp-starter",
+    href: "https://github.com/connext/xapp-starter",
+    icon: CodeIcon,
+  },
+  {
+    title: "nxtp",
+    href: "https://github.com/connext/nxtp",
+    icon: CodeIcon,
+  },
+];
